3538765f705e9504989965f3a305b1ed4cdd2088
KentonMewling <tjwright1228@gmail.com>
2023-03-05 20:57:56 -0800

    up to 12 is done

--- js/lab6-1.js
+++ js/lab6-1.js
@@ -1,2 +1,25 @@
 /* jQuery and JavaScript code here for lab6-1.html */
 
+$(document).ready(function () {
+    var title = $("#pageTitle");
+    title.html("Lab 6 - DOM Manipulation With jQuery");
+
+    var text = $("#msgArea");
+    text.html("my class is " + $("#msgArea").attr("class")); 
+
+    var bttn = $(".btn-primary");
+    bttn.css("background-color", "red");
+
+    var body = $("body");
+    body.css("background-color", "ivory");
+
+    var panel = $(".center-icons");
+    panel.addClass("selected");
+
+
+    $(".panel").click(function(){
+        $("#message").html("You clicked this panel");
+    });
+
+    });  
+
